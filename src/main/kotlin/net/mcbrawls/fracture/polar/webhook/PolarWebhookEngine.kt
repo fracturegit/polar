e4175d7afb30ccbe3bdd8bddeeaca3192ab6ac45
Daniel Orr <orrdaniel73@gmail.com>
2026-01-24 01:20:16 +0000

    fix: remove test polar endpoint

--- src/main/kotlin/net/mcbrawls/fracture/polar/webhook/PolarWebhookEngine.kt
+++ src/main/kotlin/net/mcbrawls/fracture/polar/webhook/PolarWebhookEngine.kt
@@ -9,9 +9,7 @@ import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.request.receiveChannel
 import io.ktor.server.response.respond
-import io.ktor.server.response.respondText
 import io.ktor.server.routing.RoutingContext
-import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import io.ktor.utils.io.toByteArray
@@ -51,9 +49,6 @@ class PolarWebhookEngine(private val secret: String, polarApiKey: String, polarA
 
         server = embeddedServer(Netty, port) {
             routing {
-                get("/polar") {
-                    call.respondText("Yes!")
-                }
                 post("/polar") { handleRequest() }
             }
         }
